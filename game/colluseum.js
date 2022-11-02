a302fbe3fe88ab8861be23b3aa0161b2417eb5d6
Ashur <ashursharp@ncblazers.org>
2022-11-02 08:45:22 -0500

    Game

---
+++ game/colluseum.js
@@ -0,0 +1,5 @@
+const colluseum = {
+    "floor": "sand",
+    "shape": "circle",
+    "fighters": []
+}
---
+++ game/fighter.js
@@ -0,0 +1,7 @@
+var fighter = {
+    "name":null, 
+    "hp":0,
+    "armor":0,
+    "attack":0,
+    
+}
