e71574cda3680c6c87bc92aeb6c66a387f08ad35
Ashur <ashursharp@ncblazers.org>
2022-11-15 08:08:11 -0600

    NEW

--- game/colluseum.js
+++ game/colluseum.js
@@ -1,5 +1,35 @@
 const colluseum = {
     "floor": "sand",
     "shape": "circle",
-    "fighters": []
+    "fighters": [],
+    "firstPlayersTurn":true
+}
+colluseum.load = function(){
+this.fighters[0] = fighter.new("Trump", 10, 3, 6);
+this.fighters[1] = fighter.new("Biden", 3, 9, 1);
+
+}
+
+
+
+
+colluseum.go =function(){ 
+let fighter1 = this.fighters[0];
+let fighter2 = this.fighters[1];
+while(fighter1.hp > 0 && fighter2.hp > 0){  
+    if(this.firstPlayersTurn){
+        fighter1.hp = fighter1.hp -2;
+        if(fighter1.hp <= 0){
+         document.getElementById("fighter2").classList.add("dead");
+        }
+    }else{
+       fighter2.hp = fighter1.hp -2;
+       if(fighter2.hp <= 0){
+        document.getElementById("fighter2").classList.add("dead");
+       }
+    }  
+    this.firstPlayersTurn = !this.firstPlayersTurn;
+    console.log("1:"+fighter1.hp+"  2:"+fighter2.hp);
+}
+    alert("gameover");
 }
--- game/fighter.js
+++ game/fighter.js
@@ -3,5 +3,16 @@ var fighter = {
     "hp":0,
     "armor":0,
     "attack":0,
+
+}
+fighter.new=function(name, hp, armor, attack){
+    return Object.assign({}, fighter, {name, hp, armor, attack});
+}
+
+
+fighter.defend = function(incoming){
+let damage = incoming - this.armor;
+if(damage > 0){
     
+}
 }
